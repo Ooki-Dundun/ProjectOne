11d11ae24a672cb4bd7e38842b8a2e6a1255d0de
Florian Jam <jam.florian@gmail.com>
2021-02-17 17:25:55 +0100

    revised colliding function and added blocking function for move right and move left

--- js/DOM.js
+++ js/DOM.js
@@ -57,10 +57,10 @@ const mapJS = [
     [ 1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
     [ 1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
     [ 1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
-    [ 1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
+    [ 1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 29, 30, 31, 32, 33, 34, 35, 36, 37, 'o', 'o', 'o', 'o', 'o', 'o', 'o', 45, 46, 47, 48, 49, 50],
     [ 1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
     [ 1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
-    [ 1, 2, 3, 'b' , 5, 6, 7, 8, , 10, 11, 12, 'b', 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+    [ 1, 2, 3, 4, 5, 6, 'o', 'o', 'o', 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 'b', 'b', 'b', 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
 ]
 
 // access div map container
@@ -68,14 +68,14 @@ const map = document.querySelector("#map-container");
 // insert small square in the HTML for each number in array
 mapJS.forEach(n => n.forEach(e => {
     if (typeof e === 'number') { map.innerHTML += `<div class="map-square"></div>`}
-    if (e === 'b') { map.innerHTML += `<div class="map-block"></div>`}
-    if (e === 'o') {map.innerHTML += `<div class="map-o"></div>`}
-    if (e === 'ts') {map.innerHTML += `<div class="map-ts" id="${e}"></div>`}
-    if (e === 'te') {map.innerHTML += `<div class="map-te" id="${e}"></div>`}
+    if (e === 'b') { map.innerHTML += `<div class="map-block n"></div>`}
+    if (e === 'o') {map.innerHTML += `<div class="map-block o"></div>`}
+    if (e === 'ts') {map.innerHTML += `<div class="map-block ts" id="${e}"></div>`}
+    if (e === 'te') {map.innerHTML += `<div class="map-block ta" id="${e}"></div>`}
     }));
 
 //Access map items
-
+var floor = document.getElementById('floor');
 var mapBlocks = document.getElementsByClassName('map-block');
 var mapO = document.getElementsByClassName('map-o');
 var mapTS = document.getElementsByClassName('map-ts');
@@ -91,8 +91,9 @@ const initialY = initialPosition.y;
 var isJumping = false;
 var isFalling = false;
 var colliding = false;
-var canGoRight = true;
-var canGoLeft = true;
+var isMovingRight = false;
+var isMovingLeft = false;
+var isOnHardGround = true;
 
 
 // Event Listeners For Character Motion
@@ -105,18 +106,11 @@ function move(e) {
         jump();
     }
     if (e.key === 'v' ) {
-        if (!canGoLeft) {
-            console.log('cannot go left!')
-        } else {
         moveLeft();
-        }
+
     } 
     if (e.key === 'b') {
-        if(!canGoRight) {
-            console.log('cannot go right!')
-        } else {
         moveRight();
-        }
     }
     if (e.key === 'c') {
         jumpLeft();
@@ -130,18 +124,20 @@ function move(e) {
 
 function moveRight() {
     if (isJumping) {
-        console.log('cannot move right while jumping or falling');
+        console.log('cannot move right while jumping or falling or while blocked!');
         return;
     }
+    isMovingRight = true;
+    isMovingLeft = false;
     var currentPositionR = getPositionId('player');
     var translateXR = currentPositionR[0] + 25 - initialX;
     var translateYR = currentPositionR[1] - initialY;
-    //check if collision
-    // on the edge of the map
-    // with a block
     player.style.transform = `translate(${translateXR}px, ${translateYR}px)`;
     player.style.transitionTimingFunction = 'linear';
     player.style.transitionDuration = '0.2s';
+    setTimeout( () => {
+        isMovingRight = false;
+    }, 200)
 };
 
 function moveLeft() {
@@ -149,6 +145,8 @@ function moveLeft() {
         console.log('cannot move left while jumping or falling');
         return;
     }
+    isMovingLeft = true;
+    isMovingRight = false;
     var currentPositionL = getPositionId('player');
     var translateXL = currentPositionL[0] - 25 - initialX;
     var translateYL = currentPositionL[1] - initialY;
@@ -156,6 +154,9 @@ function moveLeft() {
     player.style.transform = `translate(${translateXL}px, ${translateYL}px)`;
     player.style.transitionTimingFunction = 'linear'; 
     player.style.transitionDuration = '0.2s';
+    setTimeout( () => {
+        isMovingLeft = false;
+    }, 200)
 };
 
 
@@ -166,6 +167,8 @@ function jump() {
     }
     isJumping = true;
     isFalling = false;
+    isMovingLeft = false;
+    isMovingRight = false;
     var currentPositionJ = getPositionId('player');
     var translateYJ = currentPositionJ[1] - 53 - initialY;
     var translateXJ = currentPositionJ[0] - initialX;
@@ -174,11 +177,6 @@ function jump() {
     player.style.transitionTimingFunction = 'ease-out';
     var timeoutFall = setTimeout(fall, 800);
     var timeoutFalse = setTimeout(changeJumpAndFallToFalse, 1300);
-    // check if collision
-   
-    // if yes go down
-    // if yes, stop here.
-    // if not, got down
 }; 
 
 function fall() {
@@ -187,7 +185,7 @@ function fall() {
     }
     isFalling = true;
     var currentPositionF = getPositionId('player');
-    var translateYJ = currentPositionF[1] + 1100 - initialY;
+    var translateYJ = currentPositionF[1] + 53 - initialY;
     var translateXJ = currentPositionF[0] - initialX;
     player.style.transform = `translate(${translateXJ}px, ${translateYJ}px)`;
     player.style.transitionDuration = '0.5s';
@@ -207,6 +205,8 @@ function jumpRight() {
     }
     isJumping = true;
     isFalling = false;
+    isMovingLeft = false;
+    isMovingRight = false;
     var currentPositionJR = getPositionId('player');
     console.log(`current position when jumping is ${currentPositionJR}`)
     var translateYJR = currentPositionJR[1] - 53 - initialY;
@@ -214,9 +214,9 @@ function jumpRight() {
     player.style.transform = `translate(${translateXJR}px, ${translateYJR}px)`;
     player.style.transitionDuration = '0.7s';
     player.style.transitionTimingFunction = 'ease-out';
-    setTimeout(moveJRight, 600);
-    setTimeout(fallRight, 750);
-    setTimeout(changeJumpAndFallToFalse, 1400);
+    setTimeout(moveJRight, 700);
+    setTimeout(fallRight, 900);
+    setTimeout(changeJumpAndFallToFalse, 1300);
 }
 
 function moveJRight() {
@@ -232,7 +232,7 @@ function moveJRight() {
 function fallRight() {
     isFalling = true;
     var currentPositionFR = getPositionId('player');
-    var translateYFR = currentPositionFR[1] + 1100 - initialY;
+    var translateYFR = currentPositionFR[1] + 53 - initialY;
     var translateXFR = currentPositionFR[0] + 30 - initialX;
     player.style.transform = `translate(${translateXFR}px, ${translateYFR}px)`;
     player.style.transitionDuration = '0.4s';
@@ -246,15 +246,17 @@ function jumpLeft() {
     }
     isJumping = true;
     isFalling = false;
+    isMovingLeft = false;
+    isMovingRight = false;
     var currentPositionJL = getPositionId('player');
     var translateYJL = currentPositionJL[1] - 53 - initialY;
     var translateXJL = currentPositionJL[0] - 50 - initialX;
     player.style.transform = `translate(${translateXJL}px, ${translateYJL}px)`;
     player.style.transitionDuration = '0.7s';
     player.style.transitionTimingFunction = 'ease-out';
-    setTimeout(moveJLeft, 600);
-    setTimeout(fallLeft, 750);
-    setTimeout(changeJumpAndFallToFalse, 1400);
+    setTimeout(moveJLeft, 700);
+    setTimeout(fallLeft, 900);
+    setTimeout(changeJumpAndFallToFalse, 1300);
 }
 
 function moveJLeft() {
@@ -270,7 +272,7 @@ function moveJLeft() {
 function fallLeft() {
     isFalling = true;
     var currentPositionFL = getPositionId('player');
-    var translateYFL = currentPositionFL[1] + 1100 - initialY;
+    var translateYFL = currentPositionFL[1] + 53 - initialY;
     var translateXFL = currentPositionFL[0] - 30 - initialX;
     player.style.transform = `translate(${translateXFL}px, ${translateYFL}px)`;
     player.style.transitionDuration = '0.4s';
@@ -310,20 +312,34 @@ function isColliding() {
     let playerPosition = getPositionId('player');
     let mapBlocksCollidingData = getCollidingDataClassName('map-block');
     mapBlocksCollidingData.forEach( (n) => {
-        // collision on x axis right part of player
         if ( ( (playerPosition[2] >= n[2] && playerPosition[2] <= n[3]) || (playerPosition[3] >= n[2] && playerPosition[3] <= n[3]) ) 
         && ((playerPosition[4] <= n[4] && playerPosition[4] >= n[5]) || (playerPosition[5] <= n[4] && playerPosition[5] >= n[5]) ) ) {
             console.log(n);
             console.log("player is colliding!!!");
             colliding = true;
             if (isFalling) {
+                if ((playerPosition[5] <= n[4] && playerPosition[5] >= n[5])) {
                 console.log('coucou je tombe')
                 console.log(playerPosition);
                 console.log(n);
-                player.style.transform = `translate(${playerPosition[0]-initialX}px, ${playerPosition[1]-initialY - 10}px)`;
+                player.style.transform = `translate(${playerPosition[0] - initialX}px, ${playerPosition[5] - initialY - 13}px)`;
+                player.style.transitionDuration = '0s';
                 isFalling = false;
+                }
             }
-            return n;
+            if (isMovingRight || isMovingLeft) {
+                    if ( (playerPosition[2] >= n[2] && playerPosition[2] <= n[3]) && isMovingLeft ) {
+                            console.log('cannot move left!');
+                            player.style.transform = `translate(${playerPosition[0] - initialX}px, ${playerPosition[1] - initialY}px)`
+                            player.style.transitionDuration = '0s';
+                    }
+                    if ( (playerPosition[3] >= n[2] && playerPosition[3] <= n[3]) && isMovingRight ) {
+                            console.log("cannot move right!");
+                            player.style.transform = `translate(${playerPosition[0] - initialX}px, ${playerPosition[1] - initialY}px)`
+                            player.style.transitionDuration = '0s';
+                    }
+            }
+                
         } else {
             colliding = false;
             return;
@@ -331,7 +347,15 @@ function isColliding() {
     });
 }
 
-var checkCollision = setInterval(isColliding, 1)
+function checkIfOnHardGround() {
+    let playerPosition = getPositionId('player');
+    
+}
+
+var checkCollision = setInterval( () => {
+  isColliding();
+  checkIfOnHardGround();
+}, 1);
 
 // set interval
 
